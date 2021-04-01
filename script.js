1a73a3723a030213c5c94bae6c599b1a4cae8cd3
pmaycomia <pmaycomia@gmail.com>
2021-04-01 21:44:07 +0200

    Add exercises 4-6 of basic algorithm

--- script.js
+++ script.js
@@ -25,5 +25,28 @@ function findLongestWordLength(str) {
     return str.length;
 }
 
+function findLongestWordLength(str) {
+    let newArr = str.split(" ");
+    let result = 0;
+  
+    for (let string of newArr) {
+        if (string.length > result) {
+          result = string.length;
+        }                      
+    };
+  
+    return result;
+}
+
+function largestOfFour(arr) {
+    let newArr = [];
+   
+    for (let arrays of arr) {    
+      newArr.push(Math.max(...arrays))
+    }
+  
+    return newArr;
+}
+  
   
   
